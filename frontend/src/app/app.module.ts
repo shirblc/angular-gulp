8d0314393115e9133c38e443b6e6c88cd55a23f0
Shir Bar Lev <shiruchii@gmail.com>
2020-05-14 16:26:32 +0300

    Added base Angular files.
    -Set up the main app module.
    -Added environment configuration files.

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppComponent } from './app.component'
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}

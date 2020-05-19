fb9c7651a0e98c17ef1a6ef678e02f4644214fd9
Shir Bar Lev <shiruchii@gmail.com>
2020-05-19 17:23:17 +0300

    Changed app structure.
    -Replaced the component directive with a router directive.
    -Added routes to main component.
    -Imported the routing module to the app module.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  navTabs = [{
+    name: 'Home',
+    link: '/'
+  }];
+
+  constructor() {
+
+  }
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
+import { AppRoutingModule } from './app-routing.module'
+
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [
+
+  ],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

864b369173a0d0f0f0758f96e8f678e7c4f2e4a6
Shir Bar Lev <shiruchii@gmail.com>
2020-06-11 19:58:01 +0300

    Added compression to the frontend server.

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
 
+app.use(compression());
 app.use(express.static('./dist/'));
 
 app.get('/*', function(req, res) {

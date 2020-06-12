14e95f225feaa6704ec781c781fd9af0a371065e
Shir Bar Lev <shiruchii@gmail.com>
2020-06-12 14:52:05 +0300

    Added instructions for config file.

--- env-config.js
+++ env-config.js
@@ -1,12 +1,15 @@
 const dotenv = require('dotenv').config();
 var fs = require('fs')
 
+// Environment files can be accessed via process.env.<var_name>; here you can
+// replace static text with your Heroku environment variables. Since this runs
+// at build time, the variables will be replaced with your configured values.
 fs.readFile('dist/app.bundle.min.js', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(/{production:e.env.production,auth0:{domain:e.env.auth0domain,clientID:e.env.auth0client,audience:e.env.audience,redirectUri:e.env.loginurl,logoutUri:e.env.logouturl},backend:{domain:e.env.apiurl}}/g,
-    `{production:${process.env.production},auth0:{${domain:process.env.auth0domain},clientID:${process.env.auth0client},audience:${process.env.audience},redirectUri:${process.env.loginurl},logoutUri:${process.env.logouturl}},backend:{${domain:process.env.apiurl}}}`);
+  var result = data.replace(/ENV_VARS/g,
+    `<ENV_VARS>`);
 
   fs.writeFile('dist/app.bundle.min.js', result, 'utf8', function (err) {
      if (err) return console.log(err);

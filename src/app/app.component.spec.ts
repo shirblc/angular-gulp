22fc80d2c312bed340e2b5f39156948da4211cba
Shir Bar Lev <shiruchii@gmail.com>
2020-07-29 17:24:38 +0300

    Fixed a bug where ngModule was undefined.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,18 @@ import {} from 'jasmine';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from "./app.component";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from "@angular/platform-browser-dynamic/testing";
+
 
 describe("AppComponent", () => {
     beforeEach(fakeAsync(() => {
+      TestBed.resetTestEnvironment();
+      TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+          platformBrowserDynamicTesting());
+
       TestBed.configureTestingModule({
         imports: [
           RouterTestingModule

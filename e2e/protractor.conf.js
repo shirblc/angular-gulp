ea08e078e99874434074594402f0aaed9cc5f149
Shir Bar Lev <shiruchii@gmail.com>
2020-10-02 20:17:16 +0300

    Added Protractor config.

---
+++ e2e/protractor.conf.js
@@ -0,0 +1,40 @@
+const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
+const puppeteer = require('puppeteer');
+
+exports.config = {
+  specs: [
+    './src/**/*.spec.ts'
+  ],
+  exclude: ['**/node_modules/**'],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      binary: puppeteer.executablePath(),
+      args: [
+        '--disable-gpu',
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-extensions',
+        '--disable-dev-shm-usage'
+      ]
+    }
+  },
+  allScriptsTimeout: 11000,
+  logLevel: 'ERROR',
+  directConnect: true,
+  seleniumAddress: 'http://localhost:4444',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {},
+    random: true
+  },
+  onPrepare() {
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: StacktraceOption.PRETTY
+      }
+    }));
+  }
+};
